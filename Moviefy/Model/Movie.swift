c30b5cd7cc839fa6fe620066cb391ec45602076e
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2021-10-10 16:24:20 -0700

    Create codable model

--- Moviefy/Model/Movie.swift
+++ Moviefy/Model/Movie.swift
@@ -15,3 +15,44 @@ struct Movie {
     let releaseDate: String
 }
 
+// properties within a Movie returned from the API that we want to extract the info from
+extension Movie: Codable {
+
+    enum MovieCodingKeys: String, CodingKey {
+        case id
+        case posterPath = "poster_path"
+        case title
+        case releaseDate = "release_date"
+    }
+
+    init(from decoder: Decoder) throws {
+        // Decode each of the properties from the API into the appropriate type (string, etc.) for their associated struct variable
+        let movieContainer = try decoder.container(keyedBy: MovieCodingKeys.self)
+        id = try movieContainer.decode(Int.self, forKey: .id)
+        posterPath = try movieContainer.decode(String.self, forKey: .posterPath)
+        title = try movieContainer.decode(String.self, forKey: .title)
+        releaseDate = try movieContainer.decode(String.self, forKey: .releaseDate)
+    }
+}
+
+struct MovieApiResponse {
+    let page: Int
+    let numberOfPages: Int
+    let movies: [Movie]
+}
+
+extension MovieApiResponse: Codable {
+
+    private enum MovieApiResponseCodingKeys: String, CodingKey {
+        case page
+        case numberOfPages = "total_pages"
+        case movies = "results"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: MovieApiResponseCodingKeys.self)
+        page = try container.decode(Int.self, forKey: .page)
+        numberOfPages = try container.decode(Int.self, forKey: .numberOfPages)
+        movies = try container.decode([Movie].self, forKey: .movies)
+    }
+}
