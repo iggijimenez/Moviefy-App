adae9978c2bbc2f7d7aacb407f9a2041ce7d8c5a
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2021-10-10 16:31:04 -0700

    Configure the Request

--- Networking/Request.swift
+++ Networking/Request.swift
@@ -8,22 +8,47 @@
 
 import Foundation
 
-public enum HTTPMethod: String{
+struct Request {
+  public enum HTTPMethod: String{
     case get = "GET"
     case post = "POST"
     case put = "PUT"
     case patch = "PATCH"
     case delete = "DELETE"
-}
-
-public enum Route: String{
+  }
+  
+  public enum Route: String{
     case movies = "discover/movie"
-}
-
-struct Request {
-    static let headers = [
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": "Bearer YOUR_API_KEY"
-    ]
+  }
+  
+  static let headers = [
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Authorization": "Bearer a39a35f0ad63375d1cf21515e56de8c5"
+  ]
+  
+  static func configureRequest(from route: Route, with parameters: [String: Any], and method: HTTPMethod, contains body: Data?) throws -> URLRequest {
+    
+    // safely unwrap URL or return error
+    guard let url = URL(string: "https://api.themoviedb.org/3/\(route.rawValue)") else { fatalError("Error while unwrapping url")}
+    var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+    request.httpMethod = method.rawValue
+    request.httpBody = body
+    try configureParametersAndHeaders(parameters: parameters, headers: headers, request: &request)
+    return request
+  }
+  
+  static func configureParametersAndHeaders(parameters: [String: Any]?,
+                                            headers: [String: String]?,
+                                            request: inout URLRequest) throws {
+    do {
+      if let headers = headers, let parameters = parameters {
+        try Encoder.encodeParameters(for: &request, with: parameters)
+        try Encoder.setHeaders(for: &request, with: headers)
+      }
+    } catch {
+      throw NetworkError.encodingFailed
+    }
+  }
+  
 }
